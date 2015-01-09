8367322501a35ff5cbea0011cd499cd13b7f11a7
Jouni Vepsäläinen <jovepsal@cs.helsinki.fi>
2015-01-09 09:56:42 +0200

    removed debs

--- squid/build.sh
+++ squid/build.sh
@@ -1,3 +1,4 @@
 cd squid3-3.3.8.spaceify
+# dh_make -m --createorig # select m (multiple binary)
 dpkg-buildpackage -i.svn -us -uc
 cd ..
--- squid/remold.sh
+++ squid/remold.sh
@@ -1,24 +1,21 @@
 sudo rm squid3_3.3.8.spaceify_i386.changes > /dev/null 2>&1 || true
 sudo rm squid_3.3.8.spaceify_i386.deb > /dev/null 2>&1 || true
 sudo rm squid-cgi_3.3.8.spaceify_i386.deb > /dev/null 2>&1 || true
-sudo rm squid3_3.3.8.spaceify.debian.tar.gz > /dev/null 2>&1 || true
 sudo rm squid3_3.3.8.spaceify_i386.deb > /dev/null 2>&1 ||  true
-sudo rm squid3-common_3.3.8.spaceify_all.deb > /dev/null 2>&1 || true
 sudo rm squidclient_3.3.8.spaceify_i386.deb > /dev/null 2>&1 || true
-sudo rm squid3_3.3.8.spaceify.dsc > /dev/null 2>&1 || true
-sudo rm squid3_3.3.8.spaceify.orig.tar.xz > /dev/null 2>&1 || true
 sudo rm squid3-dbg_3.3.8.spaceify_i386.deb > /dev/null 2>&1 || true
 sudo rm squid-purge_3.3.8.spaceify_i386.deb > /dev/null 2>&1 || true
 
 sudo rm squid3_3.3.8.spaceify_amd64.changes > /dev/null 2>&1 || true
 sudo rm squid_3.3.8.spaceify_amd64.deb > /dev/null 2>&1 || true
 sudo rm squid-cgi_3.3.8.spaceify_amd64.deb > /dev/null 2>&1 || true
-sudo rm squid3_3.3.8.spaceify.debian.tar.gz > /dev/null 2>&1 || true
 sudo rm squid3_3.3.8.spaceify_amd64.deb > /dev/null 2>&1 ||  true
-sudo rm squid3-common_3.3.8.spaceify_all.deb > /dev/null 2>&1 || true
 sudo rm squidclient_3.3.8.spaceify_amd64.deb > /dev/null 2>&1 || true
-sudo rm squid3_3.3.8.spaceify.dsc > /dev/null 2>&1 || true
-sudo rm squid3_3.3.8.spaceify.orig.tar.xz > /dev/null 2>&1 || true
 sudo rm squid3-dbg_3.3.8.spaceify_amd64.deb > /dev/null 2>&1 || true
 sudo rm squid-purge_3.3.8.spaceify_amd64.deb > /dev/null 2>&1 || true
 
+sudo rm squid3_3.3.8.spaceify.debian.tar.gz > /dev/null 2>&1 || true
+sudo rm squid3-common_3.3.8.spaceify_all.deb > /dev/null 2>&1 || true
+sudo rm squid3_3.3.8.spaceify.dsc > /dev/null 2>&1 || true
+sudo rm squid3_3.3.8.spaceify.orig.tar.xz > /dev/null 2>&1 || true
+sudo rm squid3_3.3.8.spaceify.tar.gz

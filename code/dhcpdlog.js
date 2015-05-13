74ae8e8acf0ed26ec3bd06e1834c5b485e96f00b
Jouni Vepsäläinen <jovepsal@cs.helsinki.fi>
2015-05-13 17:35:21 +0300

    version 0.2.0

---
+++ code/dhcpdlog.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+/**
+ * DHCPDLog, 13.5.2015 Spaceify Inc.
+ *
+ * @class DHCPDLog
+ */
+
+var fibrous = require("fibrous");
+var PubSub = require("./pubsub");
+var Config = require("./config")();
+
+function DHCPDLog()
+{
+var self = this;
+
+var pubSub = new PubSub();
+
+self.saveToFile = function(type, ip, mac_or_duid, hostname)
+	{
+	pubSub.open(Config.LEASES_PATH);
+
+	var timestamp = Date.now();
+
+	leases = pubSub.value("leases") || {};												// current ip to mac leases
+	leases[ip] = {mac_or_duid: mac_or_duid, type: type, hostname: hostname, timestamp: timestamp};
+	pubSub.publish("leases", leases);
+
+	log = pubSub.value("leaseslog") || {};												// keep a log of connections
+	if(log[mac_or_duid])
+		log[mac_or_duid]["timestamps"].push({ts: timestamp, type: type});
+	else
+		log[mac_or_duid] = {hostname: hostname, timestamps: [{ts: timestamp, type: type}]};
+	pubSub.publish("leaseslog", log);
+
+	pubSub.close();
+	}
+
+self.getDHCPLeaseByIP = function(ip)
+	{
+	var leases = pubSub.value("leases", Config.LEASES_PATH);
+	return (leases && leases[ip] ? leases[ip] : null);
+	}
+
+}
+
+module.exports = DHCPDLog;
+
+if(process.argv.length == 6)																// isc-dhcp-server server called this script
+	{
+	dhcpd = new DHCPDLog();
+	dhcpd.saveToFile(process.argv[2], process.argv[3], process.argv[4], process.argv[5]);
+	}
--- data/scripts/rundhcp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Called from upstart script /etc/init/spaceifydhcp.conf to start Spaceify's DHCP server.
-# Spaceify Inc. 21.4.2015
-
-cd /var/lib/spaceify/code
-node dhcpservice >> /var/log/spaceifydhcp.log 2>&1
